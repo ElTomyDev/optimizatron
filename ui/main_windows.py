923a0dda7656ef1f72e1a5e68c190d229fde6533
ElTomyDev <eltomyprog@gmail.com>
2024-08-07 19:23:19 -0300

    Se optimizo el codigo y se paso todos los widgets a el 'main_windows'

--- ui/main_windows.py
+++ ui/main_windows.py
@@ -1,7 +1,6 @@
 import customtkinter as ctk
 from config.app_config import WIDTH, HEIGHT, RUTA_TEMP_1, RUTA_TEMP_2
 from services.borrar_archivos import eliminar_archivos_de_una_carpeta
-from ui.widgets.panel_servicios import panel_para_deshabilitar_servicios
 
 class MainWindows(ctk.CTk):
     def __init__(self):
@@ -13,12 +12,36 @@ class MainWindows(ctk.CTk):
         self.title("Optimizatron")
         
         ### WIDGETS ###
-        self.boton_eliminar_temps = ctk.CTkButton(self, text="Eliminar archivos temporales",command=self.eliminar_archivos_temporales)
-        self.boton_eliminar_temps.place(x=0, y=0,)
+        self.main_grid = ctk.CTkFrame(self)
+        self.main_grid.pack(padx=10, pady=10)
         
-        panel_para_deshabilitar_servicios(self, True, "Deshabilitar Servicios", 2, "black", 0.5, 0.5)
+        # Boton para eliminar archivos temporales
+        self.boton_eliminar_temps = ctk.CTkButton(self.main_grid, text="Eliminar archivos temporales",command=self.eliminar_archivos_temporales)
+        self.boton_eliminar_temps.grid(row=0,column=0)
         
-        self.update_idletasks()
+        # Panel para el apartado de 'Deshabilitar servicios de windows'
+        self.panel_service_frame = ctk.CTkFrame(self.main_grid, border_width = 2, border_color = "black")
+        self.panel_service_frame.grid(row=0, column=1)
+        
+        # Titulo para el panel de 'Deshabilitar servicios de windows'
+        self.title_service_panel = ctk.CTkLabel(self.panel_service_frame, text="Deshabilitar Servicios", font=("Arial", 16))
+        self.title_service_panel.pack(padx=20, pady=(20,0))
+        
+        # Crea los checkbox para el panel de 'Deshabilitar servicios de windows'
+        self.check_box_frame = ctk.CTkFrame(self.panel_service_frame)
+        self.check_box_frame.pack(padx=10, pady=(10,20))
+        self.create_checkboxs_for_service_panel(self.check_box_frame)
+
+    def create_checkboxs_for_service_panel(self, root):
+        """
+        Crea varios CheckBoxs para el panel del apartado de 'Deshabilitar Servicios de Windows'.
+        
+        :param root: Ruta de la vista donde se ven los checkboxs.
+        """
+        for i in range(5):
+            check_box = ctk.CTkCheckBox(root,text=f"servicio {i}")
+            check_box.grid(row=i, column=0, padx=5, pady=5)
+        
         
     ### FUNCIONES PARA BOTONES ###
     def eliminar_archivos_temporales(self):
@@ -27,7 +50,8 @@ class MainWindows(ctk.CTk):
         """
         for ruta_temp in [RUTA_TEMP_1, RUTA_TEMP_2]:
             eliminar_archivos_de_una_carpeta(ruta_temp)
-        
+    
+    ### Funcion para correr el programa ###
     def run(self):
         self.mainloop()
 
